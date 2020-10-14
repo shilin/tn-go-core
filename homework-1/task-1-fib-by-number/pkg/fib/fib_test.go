8a3b86131c4cb3e8d0a1919169f537cdb17160d9
Alexei Shilin <alexei.shilin@gmail.com>
2020-10-14 16:35:58 +0700

    add empty line

--- homework-1/task-1-fib-by-number/pkg/fib/fib_test.go
+++ homework-1/task-1-fib-by-number/pkg/fib/fib_test.go
@@ -1,8 +1,9 @@
 package fib
 
 import "testing"
+
 func TestFib(t *testing.T) {
-	const want int64 = 120 
+	const want int64 = 120
 	got := Num(5)
 	if got != want {
 		t.Fatalf("пятый элемент ряда Фибоначчи %d, а ожидалось %d", got, want)
