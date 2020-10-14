b41c09c7f67b4ef96e5d423a62779118309188a7
Alexei Shilin <alexei.shilin@gmail.com>
2020-10-14 19:27:01 +0700

    replace int64 with int

--- homework-1/task-1-fib-by-number/pkg/fib/fib_test.go
+++ homework-1/task-1-fib-by-number/pkg/fib/fib_test.go
@@ -6,6 +6,6 @@ func TestFib(t *testing.T) {
 	const want = 13
 	got := Num(7)
 	if got != want {
-		t.Fatalf("седьмой элемент ряда Фибоначчи %d, а ожидалось %d", got, want)
+		t.Fatalf("пятый элемент ряда Фибоначчи %d, а ожидалось %d", got, want)
 	}
 }
