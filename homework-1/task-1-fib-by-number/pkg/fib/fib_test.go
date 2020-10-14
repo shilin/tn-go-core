3a5c31b779b16677d54f10084d00975b632b9844
Alexei Shilin <alexei.shilin@gmail.com>
2020-10-14 16:40:41 +0700

    remove type declaration for a constant in test

--- homework-1/task-1-fib-by-number/pkg/fib/fib_test.go
+++ homework-1/task-1-fib-by-number/pkg/fib/fib_test.go
@@ -3,7 +3,7 @@ package fib
 import "testing"
 
 func TestFib(t *testing.T) {
-	const want int64 = 120
+	const want = 120
 	got := Num(5)
 	if got != want {
 		t.Fatalf("пятый элемент ряда Фибоначчи %d, а ожидалось %d", got, want)
