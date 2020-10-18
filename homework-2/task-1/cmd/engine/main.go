e9a5a56af99f3654ea86dd47758bfabbb6fa75a2
Alexei Shilin <alexei.shilin@gmail.com>
2020-10-18 17:28:43 +0700

    run engine for two sites

--- homework-2/task-1/cmd/engine/main.go
+++ homework-2/task-1/cmd/engine/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"go-core.course/homework-2/task-1/pkg/spider"
+)
 
 func main() {
-	
+	sites := []string{"https://yandex.ru", "https://ya.ru"}
+	depth := 2
+
+	// result is a map of sites mapped to url to page title
+	hashMap := make(map[string]map[string]string)
+
+	for _, site := range sites {
+		data, err := spider.Scan(site, depth)
+
+		if err != nil {
+			log.Println(err)
+		}
+		hashMap[site] = data
+	}
+
+	fmt.Println(hashMap)
 }
