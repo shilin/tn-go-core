2813e19bc5600702e1ee9ba3f8d9fddc7b1e01da
Alexei Shilin <alexei.shilin@gmail.com>
2020-10-18 18:06:47 +0700

    take search word from cli to print matches for urls and page titles

--- homework-2/task-1/cmd/engine/main.go
+++ homework-2/task-1/cmd/engine/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"strings"
 
 	"go-core.course/homework-2/task-1/pkg/spider"
 )
 
 func main() {
+	word := flag.String("word", "", "a word to search in page titles and urls")
+	flag.Parse()
+	if len(*word) == 0 {
+		fmt.Println("empty word!")
+		return
+	}
+
 	sites := []string{"https://yandex.ru", "https://ya.ru"}
 	depth := 2
 
@@ -16,12 +25,19 @@ func main() {
 
 	for _, site := range sites {
 		data, err := spider.Scan(site, depth)
-
 		if err != nil {
 			log.Println(err)
 		}
 		hashMap[site] = data
 	}
 
-	fmt.Println(hashMap)
+	// check for substring in urls and titles
+	for _, v := range hashMap {
+		for url, title := range v {
+
+			if strings.Contains(url, *word) || strings.Contains(title, *word) {
+				fmt.Printf("%s - %s\n", url, title)
+			}
+		}
+	}
 }
