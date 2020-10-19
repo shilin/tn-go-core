cf15f7cb1e6e036bf4a845f21f1fe67aca650209
Alexei Shilin <alexei.shilin@gmail.com>
2020-10-19 21:08:06 +0700

    prompt for word in CLI

--- homework-2/task-1/cmd/engine/main.go
+++ homework-2/task-1/cmd/engine/main.go
@@ -1,27 +1,23 @@
 package main
 
 import (
-	"flag"
+	"bufio"
 	"fmt"
 	"log"
+	"os"
 	"strings"
 
 	"go-core.course/homework-2/task-1/pkg/spider"
 )
 
 func main() {
-	word := flag.String("word", "", "a word to search in page titles and urls")
-	flag.Parse()
-	if len(*word) == 0 {
-		fmt.Println("empty word!")
-		return
-	}
-
 	sites := []string{"https://yandex.ru", "https://ya.ru"}
 	depth := 2
 
 	// result is a map of sites mapped to url to page title
 	hashMap := make(map[string]map[string]string)
+
+	fmt.Println("Please wait, scanning sites...")
 
 	for _, site := range sites {
 		data, err := spider.Scan(site, depth)
@@ -31,12 +27,27 @@ func main() {
 		hashMap[site] = data
 	}
 
-	// check for substring in urls and titles
-	for _, v := range hashMap {
-		for url, title := range v {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Введите слово для поиска>")
+		word, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		word = strings.TrimSuffix(word, "\n")
+		if len(word) == 0 {
+			fmt.Println("empty word!")
+			continue
+		}
 
-			if strings.Contains(url, *word) || strings.Contains(title, *word) {
-				fmt.Printf("%s - %s\n", url, title)
+		// check for substring in urls and titles
+		for _, v := range hashMap {
+			for url, title := range v {
+
+				if strings.Contains(url, word) || strings.Contains(title, word) {
+					fmt.Printf("%s - %s\n", url, title)
+				}
 			}
 		}
 	}
