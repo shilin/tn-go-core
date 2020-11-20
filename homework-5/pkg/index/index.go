3bdfe3d4f4c33b2716241af85cd23622abd64bb8
Alexei Shilin <alexei.shilin@gmail.com>
2020-11-20 13:38:33 +0700

    move id choosing for btree element to a function

--- homework-5/pkg/index/index.go
+++ homework-5/pkg/index/index.go
@@ -28,46 +28,50 @@ func (d *Doc) Titlestring() string {
 	return d.Title
 }
 
+func generateID(i int, ids []int) (newI int, newID int, newIDs []int) {
+	// cycle through slice of ascenidng natural numbers sequence
+	// using remainder of division by three
+	rem := i % 3
+	i = i + 1
+	idsLen := len(ids)
+	pos := 0
+
+	// initial step - take the center of the whole
+	if rem == 0 {
+		pos = idsLen / 2
+	}
+
+	// second step - take the center of the left half
+	if rem == 1 {
+		pos = idsLen / 4
+	}
+
+	// third step - take the center of the right half
+	// next step after third is initial again...
+	if rem == 2 {
+		pos = 3 * idsLen / 4
+	}
+
+	id := ids[pos]
+	ids = del(ids, pos)
+	return i, id, ids
+}
+
 // Build produces binarytree of Docs and inverted index for words
 func Build(hm map[string]string) (*binarytree.Node, map[string][]int) {
 	indexedWords := map[string][]int{}
 
 	docs := new(binarytree.Node)
-	idsLen := len(hm)
 	i := 0
-
+	id := 0
+	idsLen := len(hm)
 	ids := make([]int, idsLen)
 	for j := 0; j < idsLen; j++ {
 		ids[j] = j
 	}
 
-	var pos int
-
 	for url, title := range hm {
-		// cycle through slice of ascenidng natural numbers sequence
-		// using remainder of division by three
-		rem := i % 3
-		i = i + 1
-
-		// initial step - take the center of the whole
-		if rem == 0 {
-			pos = idsLen / 2
-		}
-
-		// second step - take the center of the left half
-		if rem == 1 {
-			pos = idsLen / 4
-		}
-
-		// third step - take the center of the right half
-		// next step after third is initial again...
-		if rem == 2 {
-			pos = 3 * idsLen / 4
-		}
-
-		id := ids[pos]
-		ids = del(ids, pos)
-		idsLen = len(ids)
+		i, id, ids = generateID(i, ids)
 
 		newDoc := Doc{
 			ID:    id,
